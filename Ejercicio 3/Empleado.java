81d3d0d76523cb2fb24157a2558fc79976439a24
Piotrek Budzowski <piobudzows@gmail.com>
2025-02-21 11:04:35 +0100

    Create Empleado.java

---
+++ Ejercicio 3/Empleado.java
@@ -0,0 +1,61 @@
+public class Empleado {
+
+	private String nombre;
+	private int id;
+	private Departamento departamento;
+	private Genero genero;
+
+	public Empleado(String nombre, int id, Departamento departamento, Genero genero) {
+		this.nombre = nombre;
+		this.id = id;
+		this.departamento = departamento;
+		this.genero = genero;
+	}
+
+	// METODO PARA IMPRIMIR VARIANTES DE CLASE EMPLEADO
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	public void imprimirInformacion() {
+		System.out.println("\nNombre: " + nombre);
+		System.out.println("ID: " + id);
+		System.out.println("Departamento: " + departamento);
+		System.out.println("Genero: " + genero);
+	}
+
+	// METODO PARA CAMBIAR DE DEPARTAMIENTO
+
+	public void cambiarDepartamento(Departamento nuevoDepartamento) {
+		this.departamento = nuevoDepartamento;
+	}
+}
